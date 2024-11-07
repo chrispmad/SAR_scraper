851df39b23ffc94ca34d2429157d70f98d8a8a7d
johnpphelan <trjpphelan@gmail.com>
2024-11-06 16:52:03 -0800

    remove excess imports

--- functions/airtable_functions.py
+++ functions/airtable_functions.py
@@ -1,20 +1,12 @@
-#%%
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
-from webdriver_manager.chrome import ChromeDriverManager
-import pandas as pd
-import time
-import csv
-import re
-import os
 
-#%%
+from pyairtable import Api
+from airtable import Airtable
+import requests
+import pandas as pd
+import csv
+import time
+import json
+
 
 # This will pull the table from airtable and check if they already exist in the file to be uploaded
 # the url, table, headers, and params all need to be set outside of this function 
@@ -39,7 +31,6 @@ def get_existing_records():
     existing_ids = {record['fields'].get(unique_field): record['id'] for record in records if unique_field in record['fields']}
     return existing_ids
 
-#%%
 # Deletes a record from Airtable, based on the column name and the string you pass it.
 # takes the name of the column to use a unique identifier - if you delete Group - Amphibians, then they will all get deleted
 # urls etc need to be set before hand.
@@ -79,18 +70,12 @@ def delete_record_by_col(column_name, target_value):
     
     return results
 
-#%%
+
 # upload the data to the airtable table that has been identified. Make sure they are not existing records.
-def upload_data(file_to_add, unique_field, existing_records):
+def upload_data(file_to_add):
     
     for index, row in file_to_add.iterrows():
-        unique_value = row[unique_field]
-        
-        # Check if the record already exists
-        if unique_value in existing_records:
-            #print(f"Row {index} with {unique_field} '{unique_value}' already exists. Skipping...")
-            continue
-        
+           
         # Prepare the data for uploading
         data = {
             "fields": row.to_dict()
