17dfb5af8e7a921b0205efdcaefdd4c18b86efff
johnpphelan <trjpphelan@gmail.com>
2024-11-08 17:13:41 -0800

    removed test code and added functions to upload data to airtable

--- upload_to_airtable.py
+++ upload_to_airtable.py
@@ -7,114 +7,7 @@ import pandas as pd
 import csv
 import time
 import json
-
-#upload data
-# %%
-def upload_data(file_to_add, unique_field, existing_records):
-    
-    for index, row in file_to_add.iterrows():
-        unique_value = row[unique_field]
-        
-        # Check if the record already exists
-        if unique_value in existing_records:
-            #print(f"Row {index} with {unique_field} '{unique_value}' already exists. Skipping...")
-            continue
-        
-        # Prepare the data for uploading
-        data = {
-            "fields": row.to_dict()
-        }
-        
-        # Make the POST request to upload data
-        response = requests.post(url, headers=headers, data=json.dumps(data))
-        
-        # Check for success or error in the response
-        if response.status_code == 200:
-            print(f"Row {index} uploaded successfully!")
-        #else:
-        #    print(f"Error uploading row {index}: {response.json()}")
-
-        # Sleep to respect rate limits of the Airtable API
-        time.sleep(0.2)
-
-#get existing records 
-#%%
-# This will pull the table from airtable and check if they already exist in the file to be uploaded
-# the url, table, headers, and params all need to be set outside of this function 
-# the unique field is the parameter that you are searching for - again set outside the function.
-def get_existing_records():
-    records = []
-    offset = None
-    
-    while True:
-        params = {"offset": offset} if offset else {}
-        response = requests.get(url, headers = headers, params = params)
-        if response.status_code !=200:
-            print("Error fetching records: ", response.json())
-            return records
-        data = response.json()
-        records.extend(data['records'])
-        
-        offset = data.get("offset")
-        if not offset:
-            break
-    # Extract the unique field from each record
-    existing_ids = {record['fields'].get(unique_field): record['id'] for record in records if unique_field in record['fields']}
-    return existing_ids
-
-
-#delete records
-# %%
-
-
-def delete_record_by_col(column_name, target_value):
-    
-     # Airtable formula to filter records with the target value in the specified column
-    params = {
-        'filterByFormula': f"{{{column_name}}} = '{target_value}'"
-    }
-    
-     # Fetch records that match the target value
-    response = requests.get(url, headers=headers, params=params)
-    if response.status_code != 200:
-        print("Error fetching records:", response.json())
-        return response.json()
-    
-    data = response.json()
-    records = data.get("records", [])
-    
-    # If no records match the target value
-    if not records:
-        print(f"No records found with {column_name} = '{target_value}'.")
-        return {"message": "No matching records found."}
-    
-    results = {}
-    for record in records:
-        record_id = record['id']
-        delete_url = f"{url}/{record_id}"
-        delete_response = requests.delete(delete_url, headers=headers)
-        
-        if delete_response.status_code == 200:
-            print(f"Record with ID {record_id} deleted successfully.")
-            results[record_id] = {"status": "deleted"}
-        else:
-            print(f"Error deleting record {record_id}: {delete_response.json()}")
-            results[record_id] = {"status": "error", "details": delete_response.json()}
-    
-    return results
-
-# %%
-
-fileToAdd = pd.read_csv("data/risk_registry.csv", encoding='utf-8-sig')      
-
-copydata = fileToAdd
-search_string = ["BC" , "British Columbia" , "Pacific", "British"]
-
-fileToAdd = copydata[copydata.apply(lambda row: row.str.contains(
-    '|'.join(search_string), case = False)).any(axis=1)]
-
-
-#fileToAdd = pd.read_csv("data/cosewic_spp_specialist_candidate_list.csv", encoding='ISO-8859-1')      
+import functions.airtable_functions as airfuncs
 
 with open("login/airtable_key.txt") as f:
     lines = f.readlines()
@@ -122,14 +15,12 @@ with open("login/airtable_key.txt") as f:
     token = lines[1].strip()
     print(f"USERNAME = {username}")
 
+fileToAdd = pd.read_csv("data/risk_registry.csv", encoding='utf-8-sig')      
+#fileToAdd = pd.read_csv("data/cosewic_spp_specialist_candidate_list.csv", encoding='ISO-8859-1')  
 
 base_id = 'applZn1P0abVQM8NC' # the base of the workspace - change as appropriate
-table_id = 'tblINdkiG9Nkfb44q' # change
-""" view_id = 'viwcjEPJe70stwTwe?blocks=hide' # change """
-unique_field = "Scientific name"
+table_id = 'tblmLlhGM6XKVo7WI' # Risk registry
 
-
-# %%
 # Define the API endpoint and headers
 url = f"https://api.airtable.com/v0/{base_id}/{table_id}"
 headers = {
@@ -137,59 +28,44 @@ headers = {
     "Content-Type": "application/json"
 }
 
+unique_field = "Scientific name"
+
+search_string = ["BC" , "British Columbia" , "Pacific", "British"]
+
+fileToAdd = fileToAdd[fileToAdd.apply(lambda row: row.str.contains(
+    '|'.join(search_string), case = False)).any(axis=1)]
+
+
+
+
+
+
+
+
 #with open("data/cosewic_spp_specialist_candidate_list.csv") as f:
 #    csvFile = csv.reader(f)
 #    for lines in csvFile:
 #        print(lines)
           
-#%%
+
 fileToAdd = fileToAdd.fillna('')
 if 'index' in fileToAdd.columns:
     fileToAdd['index'] = fileToAdd['index'].astype(str)
-# %%
 
-for i in range(5):
-    try:
-        index, row = next(fileToAdd.iterrows())
-        data = {"fields": row.to_dict()}
-        print(json.dumps(data))
-        
-        response = requests.post(url, headers=headers, data=json.dumps(data))
+fileToAdd = airfuncs.create_unique_id(fileToAdd)
 
-        if response.status_code != 200:
-            print(f"Error uploading row {index}: {response.json()}")
-
-        time.sleep(0.2)  # Add a delay between requests (optional)
-    except StopIteration:
-        # Reached the end of the generator (no more rows)
-        break
+# Get the current records uploaded to Airtable
+current_records = airfuncs.fetch_records(url, headers, base_id, table_id)
+# Delete all records from the current Airtable page
+if current_records:            
+    airfuncs.delete_records(url, base_id, table_id, headers, current_records)
 
 
-    
-    
-#%%
-""" for column_name in fileToAdd.columns:
-    # Create a new DataFrame with only the current column and its first 4 rows
-    column_df = fileToAdd[[column_name]].head(4)
-
-    for index, row in column_df.iterrows():
-        data = {"fields": {column_name: row[column_name]}}  # Send only the current column
-
-        print(data)
-        response = requests.post(url, headers=headers, data=json.dumps(data))
-
-        if response.status_code != 200:
-            print(f"Error uploading row {index} for column {column_name}: {response.json()}")
-
-        time.sleep(0.2)  # Add a delay between requests (optional)    """
-
-# %%
-""" delete_record_by_col("Scientific name", "Spea bombifrons") """
-
-#%%
-
+# convert all types of file to string - this should be changed later to be appropriate types
 fileToAdd = fileToAdd.astype(str)
 
+
+#upload the new file
 for index, row in fileToAdd.iterrows():
     data = {"fields": row.to_dict()}
 
@@ -201,4 +77,5 @@ for index, row in fileToAdd.iterrows():
       print(f"Error uploading row {index}: {response.json()}")
 
     time.sleep(0.2)  # Add a delay between requests (optional)
+
 # %%
